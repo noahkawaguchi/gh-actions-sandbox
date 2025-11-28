e31534ff7ba8eec213525b1d191de5e57877aef7
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2025-11-28 11:54:54 -0500

    Add math module and tests

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,9 @@
+mod math;
+
 fn main() {
     println!("Hello, world!");
+
+    let (x, y) = (2, 3);
+
+    println!("{x} + {y} = {}", math::add(x, y));
 }
---
+++ backend/src/math.rs
@@ -0,0 +1,12 @@
+pub const fn add(x: i32, y: i32) -> i32 { x + y }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn math_works_1() { assert_eq!(4, add(2, 2)) }
+
+    #[test]
+    fn math_works_2() { assert_eq!(-5, add(-8, 3)) }
+}
