40f86a3e9dfa2fd8bb8564073b2f45197e77af2c
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2025-12-06 13:30:23 -0500

    Add comment so there's a change

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod math;
 fn main() {
     println!("Hello, world!");
 
+    // This is just a comment so that there will be a change
+
     let (x, y) = (2, 3);
 
     println!("{x} + {y} = {}", math::add(x, y));
